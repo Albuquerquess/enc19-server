6d038b126ff16764dd30cf49f98112f58c8df620
Albuquerquess <albuquerque.develop@gmail.com>
2021-06-25 20:11:14 -0300

    FIX - resolve import connections

--- src/database/connections/admin/adminconnection.ts
+++ src/database/connections/admin/adminconnection.ts
@@ -1,12 +1,12 @@
-import knex from 'knex';
-import path from 'path';
+import knex from 'knex'
+import path from 'path'
 
 const adminConnection = knex({
   client: 'sqlite3',
   connection: {
     filename: path.resolve(__dirname, '..', '..', 'admin_enc19_database.sqlite')
   },
-  useNullAsDefault: true,
-});
+  useNullAsDefault: true
+})
 
-export default adminConnection;
+export default adminConnection
