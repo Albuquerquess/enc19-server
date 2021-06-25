842e80416a557c0fd0c7b9074a3b08ba62159fea
Albuquerquess <albuquerque.develop@gmail.com>
2021-06-25 20:09:16 -0300

    add imports

--- src/config/multer/multerConfig.ts
+++ src/config/multer/multerConfig.ts
@@ -1,63 +1,63 @@
-import {Request} from 'express';
-import multer from 'multer';
-import multerS3 from 'multer-s3';
-import aws from 'aws-sdk';
-import path from 'path';
-import crypto from 'crypto';
-import dotenv from 'dotenv';
+import { Request } from 'express'
+import multer from 'multer'
+import multerS3 from 'multer-s3'
+import aws from 'aws-sdk'
+import path from 'path'
+import crypto from 'crypto'
+import dotenv from 'dotenv'
 
 dotenv.config()
 
 const storageTypes = {
-    local: multer.diskStorage({
-        destination: (request: Request, file, cb) => {
-            cb(null, path.resolve(__dirname, '..', '..', '..', 'tmp', 'uploads'))
-        },
-        filename: (request: Request, file, cb) => {
-            crypto.randomBytes(16, (err, hash) => {
-                if (err) cb(err, null)
+  local: multer.diskStorage({
+    destination: (request: Request, file, cb) => {
+      cb(null, path.resolve(__dirname, '..', '..', '..', 'tmp', 'uploads'))
+    },
+    filename: (request: Request, file, cb) => {
+      crypto.randomBytes(16, (err, hash) => {
+        if (err) cb(err, null)
 
-                const fileName = `${hash.toString('hex')}-content-${file.originalname}`
+        const fileName = `${hash.toString('hex')}-content-${file.originalname}`
 
-                cb(null, fileName)
-            })
-        }
-    }),
-    s3: multerS3({
-        s3: new aws.S3(),
-        bucket: 'enc19-content-cdn',
-        contentType: multerS3.AUTO_CONTENT_TYPE,
-        acl: 'public-read',
-        key: (request: Request, file, cb) => {
-            crypto.randomBytes(16, (err, hash) => {
-                if (err) cb(err, null)
+        cb(null, fileName)
+      })
+    }
+  }),
+  s3: multerS3({
+    s3: new aws.S3(),
+    bucket: 'enc19-content-cdn',
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: 'public-read',
+    key: (request: Request, file, cb) => {
+      crypto.randomBytes(16, (err, hash) => {
+        if (err) cb(err, null)
 
-                file['nameWithHash'] = `${hash.toString('hex')}-content-${file.originalname}`
+        file.nameWithHash = `${hash.toString('hex')}-content-${file.originalname}`
 
-                cb(null, file['nameWithHash'])
-            })
-        }
-    }),
+        cb(null, file.nameWithHash)
+      })
+    }
+  })
 }
 
 export default {
-    dest: path.resolve(__dirname, '..', '..', '..', 'tmp', 'uploads'),
-    storage: storageTypes[process.env.STORAGE_TYPE],
-    limits: {
-        // not limits
-    },
-    fileFilter: (request: Request, file, cb) => {
-        const allowedMimes = [
-            'image/jpeg',
-            'image/pjpeg',
-            'image/png',
-            'image/gif'
-        ]
+  dest: path.resolve(__dirname, '..', '..', '..', 'tmp', 'uploads'),
+  storage: storageTypes[process.env.STORAGE_TYPE],
+  limits: {
+    // not limits
+  },
+  fileFilter: (request: Request, file, cb) => {
+    const allowedMimes = [
+      'image/jpeg',
+      'image/pjpeg',
+      'image/png',
+      'image/gif'
+    ]
 
-        if (allowedMimes.includes(file.mimetype)) {
-            cb(null, true)
-        }else {
-            cb(new Error('invalid file type'))
-        }
+    if (allowedMimes.includes(file.mimetype)) {
+      cb(null, true)
+    } else {
+      cb(new Error('invalid file type'))
     }
+  }
 }
